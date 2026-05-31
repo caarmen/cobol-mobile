03387ecdda82a3e10c3bfe0557cade5326bbf717
Carmen Alvarez <carmen@rmen.ca>
2026-06-01 00:14:44 +0200

    Fetch libcob from mavenlocal

--- examples/android/coboldemo/app/build.gradle.kts
+++ examples/android/coboldemo/app/build.gradle.kts
@@ -65,7 +65,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(files("src/main/libs/cob.aar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
