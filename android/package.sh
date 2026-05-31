b081f28ab5f3aa0aaa84f53ab880b75477e04228
Carmen Alvarez <carmen@rmen.ca>
2026-06-01 00:15:27 +0200

    Add maven publish script

--- android/package.sh
+++ android/package.sh
@@ -34,3 +34,38 @@ zip -r "${target_file}" AndroidManifest.xml prefab
 popd || exit
 
 echo "Created package in ${target_file}"
+
+version="0.0.1"
+mvn install:install-file \
+ -Dfile="${target_file}" \
+ -DgroupId=ca.rmen\
+ -DartifactId=gnucobol-android \
+ -Dversion="${version}" \
+ -Dpackaging=aar \
+ -DpomFile="${script_dir}/package/publish.pom"
+
+echo "Deployed to maven local"
+
+if [ "${GPGKEY}" != "" ]
+then
+  # To sign the package, use the following command to find the name of your gpg key:
+  # gpg --list-signatures --keyid-format 0xshort
+  # The name of the key is the first token after "sig 3".
+  # See https://central.sonatype.org/publish/requirements/gpg/#listing-keys for more info.
+  # Set the environment variable GPGKEY to the name of the key to use.
+  pushd "${HOME}/.m2/repository" || exit
+  publish_content_dir="ca/rmen/gnucobol-android/${version}"
+  for file in "${publish_content_dir}"/*.aar "${publish_content_dir}"/*.pom; do
+    gpg --local-user "${GPGKEY}" -ab "${file}"
+    md5 -q "${file}" > "${file}".md5
+    for algo in 1 256 512; do
+      shasum -a "${algo}" "${file}" | awk '{ print $1 }' > "${file}.sha${algo}"
+    done
+  done
+  # Create a bundle containing everything (jar, asc, pom files, and all their md5 and sha corresponding files).
+  artifact="${output_dir}/gnucobol-${version}.jar"
+  jar cfM "${artifact}" "${publish_content_dir}"/gnucobol-android*
+  popd || exit
+  echo "Publishable artifact: ${artifact}"
+fi
+
