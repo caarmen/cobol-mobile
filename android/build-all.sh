2eb2d3ea224d5076c3ee0d5cd2c9e580c5be6712
Carmen Alvarez <carmen@rmen.ca>
2026-05-31 19:09:50 +0200

    Build for multiple targets.

--- android/build-all.sh
+++ android/build-all.sh
@@ -3,6 +3,13 @@
 set -e
 
 script_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-"${script_dir}/build-gmp.sh"
-"${script_dir}/build-libdb.sh"
-"${script_dir}/build-gnucobol.sh"
+source "${script_dir}/ndk_env.sh"
+
+for target in aarch64-linux-android x86_64-linux-android; do
+    echo "Building for target ${target}"
+    export CLANG="${TOOLCHAIN}/bin/aarch64-linux-android36-clang"
+    export PREFIX_ROOT="${HOME}/.local/cobol-android-${target}"
+    "${script_dir}/build-gmp.sh"
+    "${script_dir}/build-libdb.sh"
+    "${script_dir}/build-gnucobol.sh"
+done
