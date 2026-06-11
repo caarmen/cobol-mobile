5b41e14c7992f0f02ea6e1cd47dba3e8ff5e4889
Carmen Alvarez <carmen@rmen.ca>
2026-06-11 22:00:31 +0200

    refactor: Move the cob_init() call to JNI_OnLoad.

--- examples/android/coboldemo/app/src/main/cpp/AnswerToLifeJni.c
+++ examples/android/coboldemo/app/src/main/cpp/AnswerToLifeJni.c
@@ -5,11 +5,16 @@
 
 COB_EXT_EXPORT int ANSWER__TO__LIFE(cob_u8_t *);
 
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    cob_init(0, NULL);
+
+    return JNI_VERSION_1_6;
+}
+
 jstring Java_com_example_coboldemo_AnswerToLife_cobAnswerToLife(JNIEnv *env, jobject obj) {
     cob_u8_t result[11];
     memset(result, ' ', sizeof(result));
 
-    cob_init(0, NULL);
     ANSWER__TO__LIFE(result);
 
     // Trim trailing spaces
