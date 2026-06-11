6e15ced276ce6e875971d3f11d4ba3de385095a2
Carmen Alvarez <carmen@rmen.ca>
2026-06-11 22:15:29 +0200

    feat: Call Android code from COBOL doing most JNI calls from C.
    If the answer to life is divisible by 3, show a toast with the text "Fizz".
    
    The bridge code in C is a function which uses all the jni introspection methods to get the Toast class and invoke the makeText and show methods on it.

--- examples/android/coboldemo/app/src/main/cobol/AnswerToLife.cob
+++ examples/android/coboldemo/app/src/main/cobol/AnswerToLife.cob
@@ -3,13 +3,27 @@
        DATA DIVISION.
        LINKAGE SECTION.
 
-       01  OUT-ANSWER BINARY-SHORT UNSIGNED.
+       01 IN-PTR-JNI-ENV USAGE POINTER.
+       01 IN-PTR-CONTEXT USAGE POINTER.
+       01 OUT-ANSWER BINARY-SHORT UNSIGNED.
 
        PROCEDURE DIVISION USING
-           BY REFERENCE OUT-ANSWER.
+           BY REFERENCE
+               IN-PTR-JNI-ENV
+               IN-PTR-CONTEXT
+               OUT-ANSWER.
 
            COMPUTE OUT-ANSWER = 42 *
                FUNCTION RANDOM(FUNCTION CURRENT-DATE(1:16))
+
+          *> Show a toast for some numbers
+           IF FUNCTION MOD(OUT-ANSWER, 3) = 0
+           THEN
+               CALL STATIC "showToast" USING BY REFERENCE
+                   IN-PTR-JNI-ENV
+                   IN-PTR-CONTEXT
+                   Z"Fizz!"
+           END-IF
            SET RETURN-CODE TO 1.
 
 
