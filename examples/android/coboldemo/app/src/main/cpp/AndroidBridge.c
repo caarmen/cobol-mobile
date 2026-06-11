a2980abf038601d6fec47549e30794695816da9b
Carmen Alvarez <carmen@rmen.ca>
2026-06-11 22:15:29 +0200

    feat: Call Android code from COBOL.
    If the answer to life is divisible by 3, show a toast with the text "Fizz".
    
    The bridge code in C is a function which uses all the jni introspection methods to get the Toast class and invoke the makeText and show methods on it.

---
+++ examples/android/coboldemo/app/src/main/cpp/AndroidBridge.c
@@ -0,0 +1,18 @@
+#include <jni.h>
+
+void showToast(JNIEnv *env, jobject context, const char *message) {
+    jclass toastClass = (*env)->FindClass(env, "android/widget/Toast");
+    jmethodID makeTextMethod = (*env)->GetStaticMethodID(
+            env,
+            toastClass, "makeText",
+            "(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;"
+    );
+    jstring jMessage = (*env)->NewStringUTF(env, message);
+    jobject toastObject = (*env)->CallStaticObjectMethod(
+            env,
+            toastClass, makeTextMethod, context, jMessage, 0 /* LENGTH_SHORT */
+    );
+    jmethodID showMethod = (*env)->GetMethodID(env, toastClass, "show", "()V");
+    (*env)->CallVoidMethod(env, toastObject, showMethod);
+    (*env)->DeleteLocalRef(env, jMessage);
+}
