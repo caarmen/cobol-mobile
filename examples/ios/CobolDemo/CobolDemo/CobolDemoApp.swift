67e929c903ec7ef6b0b7c277b63660a94bcfeee4
Carmen Alvarez <carmen@rmen.ca>
2026-06-13 16:00:23 +0200

    feat: Create new application from XCode.

---
+++ examples/ios/CobolDemo/CobolDemo/CobolDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobolDemoApp.swift
+//  CobolDemo
+//
+//  Created by Carmen Alvarez on 13/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CobolDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ examples/ios/CobolDemo/CobolDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CobolDemo
+//
+//  Created by Carmen Alvarez on 13/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
