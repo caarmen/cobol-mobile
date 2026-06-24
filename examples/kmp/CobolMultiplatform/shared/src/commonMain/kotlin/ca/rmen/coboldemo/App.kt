54e2a12651218b2766ad6fb821feb5b3eacbd43b
Carmen Alvarez <carmen@rmen.ca>
2026-06-25 01:12:23 +0200

    feat(kmp): Use a similar UI as the android and ios examples.

--- examples/kmp/CobolMultiplatform/shared/src/commonMain/kotlin/ca/rmen/coboldemo/App.kt
+++ examples/kmp/CobolMultiplatform/shared/src/commonMain/kotlin/ca/rmen/coboldemo/App.kt
@@ -1,11 +1,10 @@
 package ca.rmen.coboldemo
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -14,15 +13,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
 
-import cobilemultiplatform.shared.generated.resources.Res
-import cobilemultiplatform.shared.generated.resources.compose_multiplatform
 
 @Composable
 fun App(gateway: AnswerToLifeGateway) {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        var greetingText by remember { mutableStateOf("Click the button to find out...") }
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -30,21 +27,23 @@ fun App(gateway: AnswerToLifeGateway) {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { gateway.getAnswerToLife() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            Greeting(name = greetingText)
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                greetingText = "${gateway.getAnswerToLife()}"
+            }) {
+                Text("Ask COBOL")
             }
         }
     }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "The answer to life, from COBOL, is: $name!",
+        modifier = modifier
+    )
 }
 
 @Preview
