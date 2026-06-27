73d28611fa59ed524c4bb55351129d82934dd90f
Carmen Alvarez <carmen@rmen.ca>
2026-06-28 00:59:26 +0200

    build(kmp): Ran the integrateLinkagePackage gradle task.
    Ran:
    ```
    XCODEPROJ_PATH="$(pwd)/iosApp/iosApp.xcodeproj" ./gradlew :shared:integrateLinkagePackage
    ```
    
    Doc: https://kotlinlang.org/docs/multiplatform/multiplatform-spm-import.html

---
+++ examples/kmp/CobolMultiplatform/iosApp/KotlinMultiplatformLinkedPackage/subpackages/ca_rmen_gnucobol_kmp_0_0_4/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+import PackageDescription
+let package = Package(
+  name: "ca_rmen_gnucobol_kmp_0_0_4",
+  platforms: [
+    .iOS("15.0")
+  ],
+  products: [
+    .library(
+      name: "ca_rmen_gnucobol_kmp_0_0_4",
+      type: .none,
+      targets: ["ca_rmen_gnucobol_kmp_0_0_4"]
+    )
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/caarmen/cobol-mobile",
+      revision: "v0.0.3"
+    )
+  ],
+  targets: [
+    .target(
+      name: "ca_rmen_gnucobol_kmp_0_0_4",
+      dependencies: [
+        .product(
+          name: "GnuCOBOL-iOS",
+          package: "cobol-mobile"
+        )
+      ]
+    )
+  ]
+)
