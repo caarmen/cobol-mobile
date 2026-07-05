5512c8d97ebe5b6215c606275c7f5defca21678c
Carmen Alvarez <carmen@rmen.ca>
2026-07-05 22:31:28 +0200

    feat(android-example): Use the kotlin api of the android library, instead of calling cob_init directly.

--- examples/android/coboldemo/app/src/main/java/com/example/coboldemo/MainActivity.kt
+++ examples/android/coboldemo/app/src/main/java/com/example/coboldemo/MainActivity.kt
@@ -20,12 +20,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ca.rmen.gnucobol.GnuCOBOL
 import com.example.coboldemo.ui.theme.CobolDemoTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        GnuCOBOL.initialize()
         val answerToLife = AnswerToLife(this)
 
         enableEdgeToEdge()
