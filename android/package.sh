8e2bf9e9d9a74c6fd68c6af274212c9ecb4911a0
Carmen Alvarez <carmen@rmen.ca>
2026-07-08 20:24:23 +0200

    build(android-lib): Make `package.sh` build an artifact uploadable to maven central.

--- android/package.sh
+++ android/package.sh
@@ -4,6 +4,11 @@ set -e
 
 if (( "$#" != 1 )); then
   echo "Usage: $0 <gpgkey>"
+  echo "To sign the package, use the following command to find the name of your gpg key:"
+  echo "gpg --list-signatures --keyid-format 0xshort"
+  echo "The name of the key is the first token after 'sig 3'."
+  echo "See https://central.sonatype.org/publish/requirements/gpg/#listing-keys for more info."
+  echo "Set the environment variable GPGKEY to the name of the key to use."
   exit 1
 else
   gpgkey=$1
@@ -15,11 +20,6 @@ popd || exit
 
 version=0.0.5
 
-# To sign the package, use the following command to find the name of your gpg key:
-# gpg --list-signatures --keyid-format 0xshort
-# The name of the key is the first token after "sig 3".
-# See https://central.sonatype.org/publish/requirements/gpg/#listing-keys for more info.
-# Set the environment variable GPGKEY to the name of the key to use.
 for file in $(find ~/.m2/repository/ca/rmen/gnucobol-android/${version}/* -type f -not -name maven-metadata-local.xml); do
   gpg --local-user "${gpgkey}" -ab "${file}"
   md5 -q "${file}" > "${file}".md5
@@ -27,3 +27,13 @@ for file in $(find ~/.m2/repository/ca/rmen/gnucobol-android/${version}/* -type 
     shasum -a "${algo}" "${file}" | awk '{ print $1 }' > "${file}.sha${algo}"
   done
 done
+
+output_dir="$(mktemp -d)"
+pushd "${HOME}/.m2/repository" || exit
+publish_content_dir="ca/rmen/gnucobol-android/${version}"
+
+# Create a bundle containing everything (jar, asc, pom files, and all their md5 and sha corresponding files).
+artifact="${output_dir}/gnucobol-${version}.jar"
+jar cfM "${artifact}" "${publish_content_dir}"/gnucobol-android*
+popd || exit
+echo "Publishable artifact: ${artifact}"
